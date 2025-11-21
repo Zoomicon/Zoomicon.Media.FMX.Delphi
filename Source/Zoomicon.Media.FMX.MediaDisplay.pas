ee48701e7a7c2c5a39598dc2403a139ff9d56d5e
birbilis <birbilis@zoomicon.com>
2025-11-21 18:45:41 +0200

    - BOSS package version 4.9 - Added Inlining of SVG styles since SKIA SVG engine doesn't support them - Set README.md as Project page

--- Source/Zoomicon.Media.FMX.MediaDisplay.pas
+++ Source/Zoomicon.Media.FMX.MediaDisplay.pas
@@ -120,6 +120,8 @@ interface
   function ImageWrapModeToSkSvg(AMode: TImageWrapMode): TSkSvgWrapMode;
   function ImageWrapModeToSkAnimated(AMode: TImageWrapMode): TSkAnimatedImageWrapMode;
 
+  function InlineSvgStyle(const SvgText: string): string;
+
   {$ENDREGION}
 
   procedure Register;
@@ -156,6 +158,81 @@ implementation
     else
       Result := TSkAnimatedImageWrapMode.Fit; // respect TSkAnimatedImage default
     end;
+  end;
+
+  function InlineSvgStyle(const SvgText: string): string;
+  begin
+    var ResultSvg := SvgText;
+
+    // Find <style> block
+    var StyleStart := Pos('<style>', ResultSvg);
+    var StyleEnd   := Pos('</style>', ResultSvg);
+
+    if (StyleStart > 0) and (StyleEnd > StyleStart) then
+    begin
+      var StyleBlock := Copy(ResultSvg, StyleStart + Length('<style>'),
+        StyleEnd - (StyleStart + Length('<style>')));
+
+      // Split into lines/rules
+      var Lines := StyleBlock.Split([#10, #13], TStringSplitOptions.ExcludeEmpty);
+
+      for var Line in Lines do
+      begin
+        var TrimmedLine := Trim(Line);
+        // Example: .cls-1{fill:#fff;stroke:#000;stroke-width:2;}
+        if TrimmedLine.StartsWith('.') and TrimmedLine.Contains('{') then
+        begin
+          var ClassName := Copy(TrimmedLine, 2, Pos('{', TrimmedLine) - 2); // remove leading '.'
+          var RuleBody := Copy(TrimmedLine, Pos('{', TrimmedLine) + 1,
+            Pos('}', TrimmedLine) - Pos('{', TrimmedLine) - 1);
+
+          // Split properties by ';'
+          var Props := RuleBody.Split([';'], TStringSplitOptions.ExcludeEmpty);
+          var InlineAttrs := '';
+          for var Prop in Props do
+          begin
+            var Parts := Prop.Split([':'], TStringSplitOptions.ExcludeEmpty);
+            if Length(Parts) = 2 then
+            begin
+              var Name := Trim(Parts[0]);
+              var Value := Trim(Parts[1]);
+
+              // Handle shorthand stroke (e.g. "stroke: red 2px dashed")
+              if (Name = 'stroke') then
+              begin
+                var StrokeParts := Value.Split([' '], TStringSplitOptions.ExcludeEmpty);
+                if Length(StrokeParts) > 0 then
+                  InlineAttrs := InlineAttrs + ' stroke="' + StrokeParts[0] + '"';
+                if Length(StrokeParts) > 1 then
+                  InlineAttrs := InlineAttrs + ' stroke-width="' + StrokeParts[1] + '"';
+                if Length(StrokeParts) > 2 then
+                  InlineAttrs := InlineAttrs + ' stroke-dasharray="' + StrokeParts[2] + '"';
+              end
+              else if (Name = 'fill') or (Name = 'stroke-width') or
+                      (Name = 'fill-opacity') or (Name = 'stroke-opacity') or
+                      (Name = 'stroke-linecap') or (Name = 'stroke-linejoin') or
+                      (Name = 'stroke-dasharray') or (Name = 'font-family') or
+                      (Name = 'font-size') or (Name = 'font-weight') then
+                InlineAttrs := InlineAttrs + ' ' + Name + '="' + Value + '"';
+            end;
+          end;
+
+          if InlineAttrs <> '' then
+          begin
+            // Replace specific class attribute with respective inline attributes
+            ResultSvg := StringReplace(ResultSvg,
+              'class="' + ClassName + '"',
+              Trim(InlineAttrs),
+              [rfReplaceAll, rfIgnoreCase]);
+          end;
+        end;
+      end;
+
+      // Remove the <style> block entirely
+      Delete(ResultSvg, StyleStart, StyleEnd + Length('</style>') - StyleStart);
+    end;
+
+    Result := ResultSvg;
   end;
 
   {$ENDREGION}
@@ -425,7 +502,7 @@ implementation
 
   function TMediaDisplay.GetSVGText: String;
   begin
-    result := FSVGLines.Text;
+    result := FSVGLines.Text; //this always returns the original SVG text (which SetSVGText may have passed modified to SVG engine [e.g. SKIA doesn't support styles so one has to replace class attributes with inline presentation attributes])
   end;
 
   procedure TMediaDisplay.SetSVGText(const Value: String);
@@ -434,8 +511,8 @@ implementation
       SetPresenter(nil)
     else
     begin
-      FSVGLines.Text := Value; //note: reusing a single TStrings instance
-      SetSVGPresenter.Svg.Source := Value;
+      FSVGLines.Text := Value; //note: reusing a single TStrings instance //Keeping the original SVG text here
+      SetSVGPresenter.Svg.Source := InlineSvgStyle(Value); //SKIA doesn't support styles in SVG, replace class attributes with inline presentation attributes
     end;
 
     InitContent; //this does DoAutoSize (if AutoSize is set) and DoWrap
